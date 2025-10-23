e17268bd6f9ec109d789112f6ab3f33e999b7b5e
EGGYFawas <23082010080@student.upnjatim.ac.id>
2025-10-23 22:28:07 +0700

    versi lengkap {tanpa admin}
    sdhadasenua

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use App\Http\Controllers\Dokter\PatientHistoryController as DokterPatientHistory
 // [PENAMBAHAN BARU] Import Schedule Controllers
 use App\Http\Controllers\Pasien\ScheduleController as PasienScheduleController;
 use App\Http\Controllers\Dokter\ScheduleController as DokterScheduleController;
+use App\Http\Controllers\Pasien\ArticleController;
+
 
 
 /*
@@ -61,9 +63,9 @@ Route::middleware(['auth'])->group(function () {
 
         // [PENAMBAHAN BARU] Route untuk Jadwal Dokter (Pasien)
         Route::get('/jadwal-dokter', [PasienScheduleController::class, 'index'])->name('jadwal.index');
-        Route::get('/artikel', [PasienArticleController::class, 'index'])->name('artikel.index'); // Daftar artikel (dengan search)
-        Route::get('/artikel/{article:slug}', [PasienArticleController::class, 'show'])->name('artikel.show'); // Detail artikel (menggunakan slug)
-
+       // BENAR
+Route::get('/artikel', [ArticleController::class, 'index'])->name('artikel.index');
+Route::get('/artikel/{article:slug}', [ArticleController::class, 'show'])->name('artikel.show');
     });
 
     // --- GRUP ROUTE UNTUK DOKTER ---
