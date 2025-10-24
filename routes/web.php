ee3249beded484a553c0b69dd6dd554fdf891f84
EGGYFawas <23082010080@student.upnjatim.ac.id>
2025-10-25 04:39:44 +0700

    Revert "versi lengkap {tanpa admin}"
    This reverts commit e17268bd6f9ec109d789112f6ab3f33e999b7b5e.

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ use App\Http\Controllers\Dokter\PatientHistoryController as DokterPatientHistory
 // [PENAMBAHAN BARU] Import Schedule Controllers
 use App\Http\Controllers\Pasien\ScheduleController as PasienScheduleController;
 use App\Http\Controllers\Dokter\ScheduleController as DokterScheduleController;
-use App\Http\Controllers\Pasien\ArticleController;
-
 
 
 /*
@@ -63,9 +61,9 @@ Route::middleware(['auth'])->group(function () {
 
         // [PENAMBAHAN BARU] Route untuk Jadwal Dokter (Pasien)
         Route::get('/jadwal-dokter', [PasienScheduleController::class, 'index'])->name('jadwal.index');
-       // BENAR
-Route::get('/artikel', [ArticleController::class, 'index'])->name('artikel.index');
-Route::get('/artikel/{article:slug}', [ArticleController::class, 'show'])->name('artikel.show');
+        Route::get('/artikel', [PasienArticleController::class, 'index'])->name('artikel.index'); // Daftar artikel (dengan search)
+        Route::get('/artikel/{article:slug}', [PasienArticleController::class, 'show'])->name('artikel.show'); // Detail artikel (menggunakan slug)
+
     });
 
     // --- GRUP ROUTE UNTUK DOKTER ---
