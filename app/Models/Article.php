fd1d382a148c9f2bfe7acf92e177be66ab376b50
EGGYFawas <23082010080@student.upnjatim.ac.id>
2025-10-25 04:40:02 +0700

    Reapply "versi lengkap {tanpa admin}"
    This reverts commit ee3249beded484a553c0b69dd6dd554fdf891f84.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -10,24 +10,46 @@ class Article extends Model
     use HasFactory;
 
     /**
-     * The attributes that are mass assignable.
+     * Atribut-atribut yang harus di-cast.
      *
-     * @var array<int, string>
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'published_at' => 'datetime', // <-- INI PERBAIKAN ERROR 'translatedFormat on string'
+        // Mungkin Anda punya cast lain di sini, biarkan saja
+        // 'email_verified_at' => 'datetime', // (Contoh)
+    ];
+
+    /**
+     * Atribut lain yang mungkin sudah ada di model Anda
+     * (fillable, guarded, dll) biarkan saja.
      */
     protected $fillable = [
         'title',
-        'slug',
         'content',
-        'author_id',
+        'slug',
+        'image_url',
         'published_at',
+        'author_id', // Ditambahkan dari kode Anda
+        // ... atribut lain
     ];
 
     /**
      * Mendapatkan data user (penulis) dari artikel ini.
+     * (Dari kode yang Anda berikan)
      */
     public function author()
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    /**
+     * [WAJIB ADA JIKA MENGGUNAKAN SLUG DI ROUTE]
+     * Mengubah parameter binding default dari 'id' menjadi 'slug'.
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
 
